d74efe218633f18511809f504cb645fd8584c215
nagaraj madamshetti <nagarajumadamshetti@gmail.com>
2022-10-05 00:59:08 -0400

    Add user dashboard component

---
+++ src/frontend/src/components/home.js
@@ -0,0 +1,169 @@
+import './home.css'
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    HistoryOutlined,
+    SettingOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import { connect } from "react-redux";
+
+import Profile from './profile';
+import History from './history';
+import Settings from './settings'
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const [content, setContent] = useState('history');
+    const items3 = [{
+        key: `profile`,
+        icon: React.createElement(UserOutlined),
+        label: `PROFILE`,
+        onClick: () => setContent('profile'),
+    },
+    // {
+    //     key: `history`,
+    //     icon: React.createElement(HistoryOutlined),
+    //     label: `HISTORY`,
+    //     onClick: () => setContent('history'),
+    // },
+    {
+        key: `settings`,
+        icon: React.createElement(SettingOutlined),
+        label: `SETTINGS`,
+        onClick: () => setContent('settings'),
+    }]
+    return (
+        <Layout>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    // defaultSelectedKeys={['profile']}
+                    items={items3}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                >
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: 'trigger',
+                        onClick: () => setCollapsed(!collapsed),
+                    })}
+                </Header>
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                    }}
+                >
+                    {content === 'profile' && <Profile />}
+                    {/* {content === 'history' && <History />} */}
+                    {content === 'settings' && <Settings />}
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+
+
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // onSubmitLoginAction,
+    }
+}
+const mapStateToProps = state => ({
+    // user_id,
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+//   export default App;
+
+
+//   export default App;
+// import { LaptopOutlined, NotificationOutlined, UserOutlined, HistoryOutlined, SettingOutlined } from '@ant-design/icons';
+// import { Breadcrumb, Layout, Menu } from 'antd';
+// import React , { useState } from 'react';
+// import {
+//     MenuFoldOutlined,
+//     MenuUnfoldOutlined,
+//     UploadOutlined,
+//     VideoCameraOutlined,
+//   } from '@ant-design/icons';
+// const { Header, Content, Footer, Sider } = Layout;
+// const items1 = ['1', '2', '3'].map((key) => ({
+//     key,
+//     label: `nav ${key}`,
+// }));
+
+// const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+//     const key = String(index + 1);
+//     return {
+//         key: `sub${key}`,
+//         icon: React.createElement(icon),
+//         label: `subnav ${key}`,
+//         children: new Array(4).fill(null).map((_, j) => {
+//             const subKey = index * 4 + j + 1;
+//             return {
+//                 key: subKey,
+//                 label: `option${subKey}`,
+//             };
+//         }),
+//     };
+// });
+
+// const App = () => {
+//     const [collapsed, setCollapsed] = useState(false);
+//     return (
+//       <Layout>
+//         <Sider trigger={null} collapsible collapsed={collapsed}>
+//           <div className="logo" />
+//           <Menu
+//             theme="dark"
+//             mode="inline"
+//             defaultSelectedKeys={['1']}
+//             items={items3}
+//           />
+//         </Sider>
+//         <Layout className="site-layout">
+//           <Header
+//             className="site-layout-background"
+//             style={{
+//               padding: 0,
+//             }}
+//           >
+//             {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+//               className: 'trigger',
+//               onClick: () => setCollapsed(!collapsed),
+//             })}
+//           </Header>
+//           <Content
+//             className="site-layout-background"
+//             style={{
+//               margin: '24px 16px',
+//               padding: 24,
+//               minHeight: 280,
+//             }}
+//           >
+//             Content
+//           </Content>
+//         </Layout>
+//       </Layout>
+//     );
+//   };
+
+// export default App;

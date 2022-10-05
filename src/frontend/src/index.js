4c2132d6f5d1bebe2f035a51210fa24e6b7caf07
nagaraj madamshetti <nagarajumadamshetti@gmail.com>
2022-10-05 01:02:04 -0400

    Update index.js to add dependency css file

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import store from './app/store';
 import LeftOver from './leftOver';
 import './index.css';
+import 'antd/dist/antd.css';
 
 const root = createRoot(document.getElementById('root'));
 

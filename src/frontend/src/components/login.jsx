743a8219a74da332b5e67734c4cebb001fcd867a
Akash Gupta <guptaa91k@gmail.com>
2022-10-08 00:44:08 -0400

    Add unit testing for login

--- src/frontend/src/components/login.jsx
+++ src/frontend/src/components/login.jsx
@@ -27,7 +27,7 @@ class LoginUser extends React.Component {
     handleSubmit = async (event) => {
         const keys = ['email', 'pass'];
         for (let i = 0; i < keys.length; i++) {
-            if (this.state[keys[i]] === '') return;
+            if (this.state[keys[i]] === '') return false;
         }
         event.preventDefault();
         const emailRegex = new RegExp('\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})');

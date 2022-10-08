d14fe92ba619c720e4b49507253c7cf4c6b82c84
nitesh31mishra <nitesh31mishra@gmail.com>
2022-10-08 19:11:10 -0400

    Update util.py functions

--- src/Backend/utils.py
+++ src/Backend/utils.py
@@ -8,8 +8,11 @@ from ast import literal_eval as make_tuple
 
 connection = mysql.connector.connect(
     host="localhost", user="root", password="", database="donationsystem")
-cursor = connection.cursor(dictionary=True)
 
+cursor = connection.cursor(dictionary=True)
+# cursor.execute('set GLOBAL max_allowed_packet=67108864')
+# connection.query('SET GLOBAL connect_timeout=6000')
+# cursor.execute('set max_allowed_packet=67108864')
 
 def get_items(page, user_id):
     """
@@ -29,22 +32,25 @@ def get_items(page, user_id):
     """
 
     try:
-
-        sql_get_data_query = """select Interests from users where ID = %s"""
+        cursor = connection.cursor(dictionary=True)
+        finalData =[]
+        sql_get_data_query = """select email, Interests from users where ID = %s"""
         record = (int(user_id),)
         cursor.execute(sql_get_data_query, record)
-        record = cursor.fetchall()
-        print(record[0]["Interests"])
+        data = cursor.fetchall()
 
-        record = make_tuple(record[0]["Interests"])
-        tuplerecord = tuple(record)
-        print(tuplerecord)
+        # data = make_tuple(data[0]["Interests"])
+        tuplerecord = tuple(make_tuple(data[0]["Interests"]))
 
         sql_select_query = """select * from items where category in {} order by item_id  limit 10 offset {} """.format(
             tuplerecord, int(page)*10-10)
         cursor.execute(sql_select_query)
         new_record = cursor.fetchall()
-        return True, new_record
+        for record in new_record:
+            finalData.append({"itemId": record["item_id"], "itemName": record["item_name"], "itemQuantity": record["quantity"], "itemDescription": record["description"],
+                              "itemZipCode": record["zipcode"], "itemCity": record["city"], "itemDonorId": record["donor_id"], "itemCategory": record["category"], "donorEmail": data[0]["email"]})
+        cursor.close()
+        return True, finalData
 
     except mysql.connector.Error as error:
         print("Failed to get record from MySQL table: {}".format(error))
@@ -87,6 +93,7 @@ def insert_item(item_name, quantity, description, zipcode, city, donor_id, categ
 
     try:
         print(item_name)
+        cursor = connection.cursor(dictionary=True)
         mysql_insert_query = """INSERT INTO items (item_name, quantity, description, zipcode, city, donor_id, category) 
                                 VALUES (%s, %s, %s, %s, %s, %s, %s) """
 
@@ -96,6 +103,7 @@ def insert_item(item_name, quantity, description, zipcode, city, donor_id, categ
         connection.commit()
         print("Record inserted successfully into item table")
         msg = "Record inserted successfully into item table"
+        cursor.close()
         return True, msg
 
     except mysql.connector.Error as error:
@@ -126,6 +134,7 @@ def update_item(data):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         mysql_update_query = """UPDATE items set item_name = %s, quantity = %s, description = %s, zipcode = %s, city = %s, donor_id = %s, category = %s WHERE item_id = %s """
 
         input_data = (data['item_name'], data['quantity'], data['description'],
@@ -134,6 +143,7 @@ def update_item(data):
         connection.commit()
         print("Record updated successfully into item table")
         msg = "Record updated successfully into item table"
+        cursor.close()
         return True, msg
 
     except mysql.connector.Error as error:
@@ -158,12 +168,18 @@ def getDonorHistory(ID):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
+        finalData=[]
         cursor.execute(
-            'SELECT item_id, recipient_id FROM Donation where donation_id = %s', (int(ID),))
+            'SELECT * FROM items where donor_id = %s', (int(ID),))
         data = cursor.fetchall()
         print(data)
+        for record in data:
+            finalData.append({"itemId": record["item_id"], "itemName": record["item_name"], "itemQuantity": record["quantity"], "itemDescription": record["description"],
+                              "itemZipCode": record["zipcode"], "itemCity": record["city"], "itemDonorId": record["donor_id"], "itemCategory": record["category"]})
         # print(record[0]["Interests"])
-        return True, data
+        cursor.close()
+        return True, finalData
     except mysql.connector.Error as error:
         msg = "Failed to get history {}".format(error)
         return False, msg
@@ -185,10 +201,11 @@ def getRecieverHistory(ID):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         cursor.execute(
             'SELECT item_id, donation_id, recipient_id FROM Donation where recipient_id = %s', (int(ID),))
         data = cursor.fetchall()
-
+        cursor.close()
         return True, data
     except mysql.connector.Error as error:
         msg = "Failed to get history {}".format(error)
@@ -213,10 +230,12 @@ def addDonation(item_id, recipient_id):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         sql_insert_query = "INSERT INTO donation (item_id, recipient_id) VALUES (%s, %s)"
         cursor.execute(sql_insert_query, (item_id, recipient_id))
         connection.commit()
         msg = msg = "Record inserted successfully into donation table"
+        cursor.close()
         return True, msg
     except mysql.connector.Error as error:
         msg = "Failed to insert into donation table {}".format(error)
@@ -239,6 +258,7 @@ def getUserProfileByID(ID):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         cursor.execute(
             'SELECT name, email, city, zipcode, interests FROM users where ID = %s', (int(ID),))
         user = cursor.fetchone()
@@ -247,6 +267,7 @@ def getUserProfileByID(ID):
         user["interests"] = ast.literal_eval(user["interests"])
 
         print(type(user))
+        cursor.close()
         return user
     except mysql.connector.Error as error:
         print(error)
@@ -269,6 +290,7 @@ def updateProfile(data):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         mysql_update_query = """UPDATE users set name = %s, email=%s, city=%s, zipcode=%s, interests=%s WHERE ID = %s """
 
         input_data = (data['name'], data['email'],
@@ -278,6 +300,7 @@ def updateProfile(data):
 
         print("Record updated successfully into item table")
         msg = "Record updated successfully into item table"
+        cursor.close()
         return True, msg
 
     except mysql.connector.Error as error:
@@ -302,6 +325,7 @@ def getUserProfileByEmail(email):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         cursor.execute(
             'SELECT ID, name, email, city, zipcode, interests FROM users WHERE email = %s', (email,))
         user = cursor.fetchone()
@@ -309,6 +333,7 @@ def getUserProfileByEmail(email):
         user["city"] = ast.literal_eval(user["city"])
         user["zipcode"] = ast.literal_eval(user["zipcode"])
         user["interests"] = ast.literal_eval(user["interests"])
+        cursor.close()
         return user
     except mysql.connector.Error as error:
         return []
@@ -332,9 +357,11 @@ def loginCheck(email, password):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         cursor.execute(
             'SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
         user = cursor.fetchone()
+        cursor.close()
         if user:
             return (True, 1)
         else:
@@ -369,10 +396,12 @@ def addUser(name, password, email, city, zipcode, interests):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         sql_insert_query = "INSERT INTO users (name, password, email, city, zipcode, interests) VALUES (%s, %s, %s, %s, %s, %s)"
         cursor.execute(sql_insert_query, (name, password,
                                           email, city, zipcode, interests))
         connection.commit()
+        cursor.close()
         return True
     except mysql.connector.Error as error:
         return False
@@ -394,11 +423,13 @@ def checkDuplicateEmail(email):
     """
 
     try:
+        cursor = connection.cursor(dictionary=True)
         sql_select_query = "SELECT * FROM users WHERE email = %s"
         cursor.execute(sql_select_query, (email,))
         # fetch result
         record = cursor.fetchall()
         match = re.match(r'[^@]+@[^@]+\.[^@]+', email)
+        cursor.close()
         if record or not match:
             return (True, 1)
         else:
